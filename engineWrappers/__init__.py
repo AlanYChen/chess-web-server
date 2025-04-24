a54f3221079585d11db2ad207eb1924f4955cf01
Alan <asdfqzq@gmail.com>
2025-04-24 15:50:44 -0700

    Hotfix

--- engineWrappers/__init__.py
+++ engineWrappers/__init__.py
@@ -1,3 +1,3 @@
-# from .models import Stockfish
-# from .models2 import Maia
-# from engineWrappers.chessRelatedExceptions import ChessEngineException
+from .models import Stockfish
+from .models2 import Maia
+from engineWrappers.chessRelatedExceptions import ChessEngineException, ChessEngineImproperInputException
