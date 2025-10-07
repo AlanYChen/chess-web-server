4efa442d9a5a05d8e66997be0c4f6cce3346f304
Alan <asdfqzq@gmail.com>
2025-10-07 11:51:58 -0700

    Add log to file

--- utils/logger.py
+++ utils/logger.py
@@ -1,5 +1,21 @@
+import sys
+import threading
+
 _LOGGING = True
 
 def log(msg):
-    if _LOGGING:
-        print(msg)
+    if not _LOGGING:
+        return
+    
+    print(msg)
+
+    # Write to file as well
+    original_stdout = sys.stdout
+    try:
+        current_thread_name = threading.current_thread().name
+
+        with open(current_thread_name + "_log.txt", "w") as f:
+            sys.stdout = f
+            print(msg)
+    finally:
+        sys.stdout = original_stdout
