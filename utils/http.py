7045639acb37182c320d8d2ff3c8f234612eab9c
Alan <asdfqzq@gmail.com>
2025-12-23 17:57:03 -0800

    Removed place id check

--- utils/http.py
+++ utils/http.py
@@ -11,9 +11,11 @@ def checkHttpRequest(request_lines):
     if request_lines[0] != "POST / HTTP/1.1":
         return False
     
-    place_id_1 = f"Roblox-Id: {TEST_GAME_PLACE_ID}"
-    place_id_2 = f"Roblox-Id: {OFFICIAL_GAME_PLACE_ID}"
+    # Decided to remove this requirement as it's unlikely to deter actual exploitation and
+    # makes place changes more inconvenient for me to deal with.
+    # place_id_1 = f"Roblox-Id: {TEST_GAME_PLACE_ID}"
+    # place_id_2 = f"Roblox-Id: {OFFICIAL_GAME_PLACE_ID}"
+    # if place_id_1 not in request_lines and place_id_2 not in request_lines:
+    #     return False
 
-    if place_id_1 not in request_lines and place_id_2 not in request_lines:
-        return False
     return True
